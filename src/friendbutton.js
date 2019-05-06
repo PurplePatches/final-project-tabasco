9dee4229406174a6de73ed55c330219009017805
Steve Frenzel <stevefrenzel@gmx.de>
2019-05-06 16:02:20 +0200

    progress week 11, day 1

---
+++ src/friendbutton.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default class FriendButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <button>FRIEND BUTTON</button>
+            </React.Fragment>
+        );
+    }
+}
